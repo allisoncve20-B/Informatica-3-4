0e10f8c33584a953020deb12655e0223195186c8
allisoncve20-B <allisoncve20@gmail.com>
2026-04-21 15:49:28 +0000

    machine stock

---
+++ vending.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Vending Machine Stock \n");
+    printf("---------------------\n");
+    printf("chips \n");
+    float price1 = 15.65;
+    int quantity1 = 20;
+    char select1 = 'c';
+    
+
+
+
+}
