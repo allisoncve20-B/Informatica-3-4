7552489a81614632d8b98827b9a49975589d17e4
allisoncve20-B <allisoncve20@gmail.com>
2026-04-21 14:56:44 +0000

    variables

---
+++ naming-variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number; // I fixed the name of this variable by deleting the exclamation mark
+int test_Value; // I fixed the name of this variable by additing underscore
+int example1; // I fixed the name of this variable by moving the number to the end of the word
+float int20; // I fixed the name of this variable by adding number
+printf("Variables created successfully.\n");
+
+}
+
