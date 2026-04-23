d6a3e93c1959386d20238b7f70c5ab3fd7ebe909
allisoncve20-B <allisoncve20@gmail.com>
2026-04-23 21:09:50 +0000

    azul hello 2.0

--- hello.c
+++ hello.c
@@ -2,5 +2,12 @@
 
 int main(void)
 {
-    printf("Hello Student Allison \n");
-}
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%s",&name);
+    printf("Hello %s!\n", name);
+    char color[50];
+    printf("Enter your favorite color:");
+    scanf("%s",&color);
+    printf("Your favorite color is %s!\n",color);
+        }
