ab864d1ff27d15e8f17434da009982fc2f4524af
allisoncve20-B <allisoncve20@gmail.com>
2026-04-24 16:39:59 +0000

    vending machine

--- vending.c
+++ vending.c
@@ -8,8 +8,31 @@ int main(void)
     float price1 = 15.65;
     int quantity1 = 20;
     char select1 = 'c';
-    
+    printf("Price = %.2f\n",price1 );
+    printf("Quantity = %d\n",quantity1);
+    printf("Selection Code = %c\n",select1);
+
+    printf("------------------------\n");
+    printf("cookies \n");
+    float price2 = 18.45;
+    int quantity2 = 16;
+    char select2= 'v';
+    printf("Price = %.2f\n",price2 );
+    printf("Quantity = %d\n",quantity2);
+    printf("Selection Code = %c\n",select2);
+
+    printf("------------------------\n");
+    printf("burgers \n");
+    float price3 = 16.99;
+    int quantity3 = 25;
+    char select3= 'b';
+    printf("Price = %.2f\n",price3 );
+    printf("Quantity = %d\n",quantity3);
+    printf("Selection Code = %c\n",select3);
 
 
 
-}
+
+
+
+    }
