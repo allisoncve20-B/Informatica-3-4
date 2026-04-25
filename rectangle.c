c0eceef16e463dc194db0a82710b1380b8fdb74c
allisoncve20-B <allisoncve20@gmail.com>
2026-04-25 03:17:15 +0000

    rectangle

--- rectangle.c
+++ rectangle.c
@@ -1,3 +1,15 @@
 #include<stdio.h>
 
 int main(void)
+ {
+    int length;
+    int width;
+    printf("Enter the length of the rectangle: ");
+    scanf("%d", &length);
+    printf("Enter the width of the rectangle: ");
+    scanf("%d", &width);
+    int area = width*length;
+    int perimenter = (width*length)*2;
+    printf("\nThe area of the rectangle is: %d\nThe perimenter of the rectangle is %d.\n",area,perimenter);
+
+ }
