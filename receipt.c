343011f4dc72283182056fb6396947b6b6315636
allisoncve20-B <allisoncve20@gmail.com>
2026-04-29 14:57:53 +0000

    test

---
+++ receipt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char item[50];
+    float Price = 2.00;
+    printf("***************************\n");
+    printf("Allis Store\n");
+    printf("***************************\n");
+    printf("Item\n");
+    scanf("%d",&item);
+    int Quantity;
+    printf("Quantity\n");
+     scanf("%d",&Quantity);
+     printf("Price\n");
+     scanf("%f",&Price);
+
+
+}
